2a9ca0582dc0625c918e5fe5d6ba7f0ccb99f7ee
Eagler <jayden.okubadej@outlook.com>
2024-07-27 19:58:58 -0500

    Start

---
+++ api/proxy.js
@@ -0,0 +1,11 @@
+const cors_proxy = require('cors-anywhere');
+
+const server = cors_proxy.createServer({
+  originWhitelist: [], // Allow all origins
+  requireHeader: ['origin', 'x-requested-with'],
+  removeHeaders: ['cookie', 'cookie2']
+});
+
+module.exports = (req, res) => {
+  server.emit('request', req, res);
+};
