9bbc6ef1bab1a57bb86578cf25acf988513f5b21
Eagler <jayden.okubadej@outlook.com>
2024-07-28 14:31:50 -0500

    fix

--- index.js
+++ index.js
@@ -5,8 +5,13 @@ const port = process.env.PORT || 8080;
 
 cors_proxy.createServer({
   originWhitelist: [], // Allow all origins
-  requireHeader: ['origin', 'x-requested-with'],
-  removeHeaders: ['cookie', 'cookie2']
+  requireHeader: [],
+  removeHeaders: ['cookie', 'cookie2'],
+  setHeaders: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  }
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
